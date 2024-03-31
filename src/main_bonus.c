7d81ea12da244d3fa8e7db3d09c209094ecfe532
haddol7 <haddol010313@khu.ac.kr>
2024-03-31 21:59:47 +0900

    [ADD] malloc nodes by argv inputs

--- src/main_bonus.c
+++ src/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: daeha <daeha@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 13:02:49 by daeha             #+#    #+#             */
-/*   Updated: 2024/03/31 17:49:04 by daeha            ###   ########.fr       */
+/*   Updated: 2024/03/31 21:59:21 by daeha            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,25 +27,43 @@ rrr |	rra && rrb
 */
 
 #include <stdio.h>
+#include "ft_printf.h"
 #include "libft.h"
 
-static const char	*move_to_num(const char *str, int *sign)
+void	leaks(void)
 {
-	while (*str != 0 && \
-	(*str == '\t' || *str == '\n' || *str == ' ' || \
-	*str == '\v' || *str == '\f' || *str == '\r'))
+	system("leaks checker");
+}
+
+void	terminate(void)
+{
+	ft_printf("Error\n");
+	exit(1);
+}
+
+static char	*move_to_num(char *str, int *sign, int *nothing)
+{
+	*nothing = 0;
+	while (*str != '\0' && *str == ' ')
 		str++;
-	if (*str == '-')
+	if (*str != '\0')
 	{
-		*sign = -1;
-		return (++str);
+		if (*str == '-' && *(str + 1))
+		{
+			*sign = -1;
+			return (++str);
+		}
+		else if (*str == '+' && *(str + 1))
+			return (++str);
+		if (*str < '0' || *str > '9')
+			terminate();
 	}
-	else if (*str == '+')
-		return (++str);
+	else
+		*nothing = 1;
 	return (str);
 }
 
-int	atoi_fit(const char *str)
+int	atoi_fit(char **str, int *nothing)
 {
 	long	result;
 	int		temp;
@@ -53,23 +71,97 @@ int	atoi_fit(const char *str)
 
 	sign = 1;
 	result = 0;
-	str = move_to_num(str, &sign);
-	while (*str >= '0' && *str <= '9')
+	*str = move_to_num(*str, &sign, nothing);
+	while (**str >= '0' && **str <= '9')
 	{
 		result *= 10;
-		result += *str - '0';
+		result += **str - '0';
 		temp = (int)result;
 		if (temp != result)
-		{
-			perror("error");
-			return (-1);
-		}
-		str++;
+			terminate();
+		(*str)++;
 	}
 	return ((int)(sign * result));
 }
 
+
+// 1. argv 인자를 받고 이를 stack a에 저장한다.
+// int main(int argc, char **argv)
+// {
+// 	t_total	stack;
+// 	char	*command;
+// 	int		is_eof;
+
+// 	atexit(leaks);
+// 	is_eof = 0;
+// 	stack.size = put_value_in_stack_a(argc, argv, &stack.a);
+// 	while (!is_eof)
+// 	{
+// 		command = get_next_line(0);
+// 		is_eof = command_to_stack(&stack, command);
+// 		free(command);
+// 	}
+// 	check_stack_sorted(stack);
+// 	return (0);
+// }
+
+size_t	put_value_in_stack_a(int argc, char **argv, t_stack *a)
+{
+	int		cnt;
+	int		result;
+	int		nothing;
+	t_node	*node;
+
+	cnt = 1;
+	while (cnt < argc)
+	{
+		result = atoi_fit(&argv[cnt], &nothing);
+		if (argv[cnt][0] == '\0')
+		{
+			cnt++;
+			if (cnt < argc && argv[cnt][0] == '\0')
+				terminate();
+		}
+		if (nothing == 0)
+		{
+			node = (t_node *)malloc(sizeof(t_node));
+			if (node == NULL)
+				exit(1);
+			node->val = result;
+			push(a, node, BOT);
+		}
+	}
+	return (a->size);
+}
+
+// test code
+/*
 int main(int argc, char **argv)
 {
-	printf("%d", atoi_fit(argv[1]));
+	t_total	stack;
+	t_node	*temp;
+
+	atexit(leaks);
+	
+	ft_memset(&stack, 0, sizeof(t_total));
+
+	stack.size = put_value_in_stack_a(argc, argv, &stack.a);
+	temp = stack.a.top;
+	for (size_t i = 0; i < stack.a.size; i++)
+	{
+		ft_printf("%dth node : %d\n", i, temp->val);
+		temp = temp->next;
+	}
+	int size = stack.a.size;
+	for (int i = 0; i < size; i++)
+	{
+		temp = pop(&stack.a, BOT);
+		free(temp);
+	}
+
+	return (0);
 }
+*/
+
+
+
